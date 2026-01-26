2647c95c8258d682da849b1028769122d156b7bf
Kitt0ny <dima0908074@mail.ru>
2026-01-26 12:27:29 +0400

    add dependency

--- src/main/java/org/example/Security/Person.java
+++ src/main/java/org/example/Security/Person.java
@@ -1,10 +1,7 @@
 package org.example.Security;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
@@ -96,7 +93,7 @@ public class Person {
         this.registrationDate = registrationDate;
     }
 
-    public PersonDTO getDTO(){
+    public PersonDTO getDTO() {
         return new PersonDTO(login, domain, role, registrationDate);
     }
 }
